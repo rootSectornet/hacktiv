aab18de17d414ffdd3cbdd89353ce446389cb50a
tediroot <tedijammz@gmail.com>
2017-08-23 23:45:30 +0700

    latest README

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
+const path = require('path')
 const express = require('express')
-
+const data = require('./data/item.json')
 const app = express()
-const middlewaresatu = (req, res, next) => {
-	console.log("middleware pertama")
-	next()
-}
-const middlewaredua = (req, res, next) => {
-	console.log(middlewaresatu)
-	next()
-}
-app.use(middlewaresatu)
+// setup view engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 
-app.use(middlewaredua)
+app.use(express.static('public'))
 
 app.get('/',(req, res) => {
-	res.send("hello Tedi susanto")
+	// res.send("hello Tedi susanto")
+	res.render('index',{
+		item : data
+	})
 })
 
-app.get('/echo/:name', (req, res) => {
-	res.send(`hello ${req.params.name}`)
-})
 app.listen(5432, () => {
 	console.log("server running at http://localhost:5432")
 })
