1fa827f432f2cdecb9a3d48e7bff0c661228b2d7
tediroot <tedijammz@gmail.com>
2017-08-24 16:40:57 +0700

    selesai website sederhana menggunakan node.js dan express

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
 const path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser')
 const data = require('./data/item.json')
 const app = express()
 // setup view engine
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
-
+app.use(bodyParser.urlencoded({
+	extended:false
+}))
 app.use(express.static('public'))
-
 app.get('/',(req, res) => {
 	// res.send("hello Tedi susanto")
 	res.render('index',{
 		item : data
 	})
 })
-
-app.listen(5432, () => {
+app.get('/detail/:id', (req, res) => {
+	const items = data.find(d => {
+		return d.id === parseInt(req.params.id)
+	})
+	res.render('post',{
+			item : items
+	})
+})
+app.listen(3000, () => {
 	console.log("server running at http://localhost:5432")
 })
